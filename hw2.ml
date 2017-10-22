954cb7ffbe396f86ab117b751782da5ff05d69dd
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-22 13:53:23 -0700

    Modify conditional in produce_al helper function to multiple lines

--- hw2.ml
+++ hw2.ml
@@ -3,7 +3,10 @@
 let rec produce_al g nts =
 	match g with
 	| [] -> []
-	| (nt, rhs)::tl -> if nt = nts then rhs@produce_al tl nts else produce_al tl nts
+	| (nt, rhs)::tl ->
+		if nt = nts then
+			rhs@produce_al tl nts
+		else produce_al tl nts
 
 let convert_grammar gram1 = 
 	(fst gram1, fun nts -> produce_al (snd gram1) nts)
